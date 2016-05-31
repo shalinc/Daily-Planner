802850b69b4b2af7d76bdb7644b12d4c17d8ec5e
shalinc <shalin.chopra@yahoo.com>
2016-05-31 18:52:44 -0500

    Changes made for edit functionality

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
 $(document).ready(function() {
+    
     // AddTask Event
     $('#add-task-form').on('submit',function(e){
         addTask(e);
       });
     
+    // Edit task Event
+    $('#edit-task-form').on('submit',function(e){
+       updateTask(e); 
+    });
+    
+    //display all the tasks on page load
     displayTasks();
     
     //Function to display the tasks
-    function displayTasks(){
+    function displayTasks()
+    {
+        //get all the tasks which is a JSON
         var taskList = JSON.parse(localStorage.getItem('tasks'));
+        
         //display task in sorted order
-        if(taskList != null){
+        if(taskList != null)
+        {
             taskList = taskList.sort(sortByTime);
         }
         
         //loop through each task and display accordingly by sorting
         var i = 0;
-        
         //check tasks
-        if (localStorage.getItem('tasks') != null){
+        if (localStorage.getItem('tasks') != null)
+        {
+            //for each task in taskList do
             $.each(taskList, function(key, value){
                 $('#task-table').append('<tr id="' + value.id +'">' +
                                         '<td>' + value.task + '</td>'+
@@ -31,31 +43,105 @@ $(document).ready(function() {
         }
         
         //Function to sort Tasks
-        function sortByTime(a, b){
+        function sortByTime(a, b)
+        {
             var aTime =  a.task_time;
             var bTime = b.task_time;
             
             return((aTime < bTime) ? -1 : ((aTime > bTime) ? 1 : 0));
-        }
+        } // sort ends
         
-        
-    }
+    } //func displayTasks ends
     
     
     //Function to add a Task
-    function addTask(e){
+    function addTask(e)
+    {
         
-        //get the Unique value i.e. the date with its timestamp
+        //get the Unique value i.e. the timestamp
         var newDate = new Date();
-        var id = newDate.getTime();
+        id = newDate.getTime();
         
+        //fetch the other elements from the HTML page using their ID's
         var task = $('#task').val();
         var task_priority = $('#priority').val();
         var task_date = $('#date').val();
         var task_time = $('#time').val();
         
         //Adding Simple Validation
+        if(task == '')
+        {
+            alert("Task field is required")
+            e.preventDefault();
+        }
+        else if(task_date == '')
+        {
+            alert("Please Select Date")
+            e.preventDefault();
+        }
+        else if(task_time == '')
+        {
+            alert("Please select Time")
+            e.preventDefault();
+        }
+        else
+        {
+            //get the tasks
+            tasks = JSON.parse(localStorage.getItem('tasks'));
+            
+            //check if null
+            if (tasks == null)
+            {
+                tasks = [];
+            }
+            
+            //Create a task list
+            //var taskList = JSON.parse(localStorage.getItem('tasks'));
+            
+            //New Task Object creation
+            var newTask = {
+                "id": id,
+                "task": task,
+                "task_priority": task_priority,
+                "task_date": task_date,
+                "task_time": task_time
+            }
+            
+            //push the task into tasks array
+            tasks.push(newTask);
+            
+            //store it onto localstorage
+            localStorage.setItem('tasks',JSON.stringify(tasks));
+        }
+        
+    }//add Task function ends
     
+    //Function to update the changed task
+    function updateTask(e)
+    {
+    
+        //fetch all the values to be updated
+        var id = $('#task-id').val();
+        var task = $('#task').val();
+        var task_priority = $('#priority').val();
+        var task_date = $('#date').val();
+        var task_time = $('#time').val();
+        
+        //fetch all the tasks
+        taskList = JSON.parse(localStorage.getItem('tasks'));
+        
+        //find the task to edit using its ID
+        for (var i=0;i<taskList.length;i++)
+        {
+            if (taskList[i].id == id)
+            {
+                taskList.splice(i,1);
+            }
+            localStorage.setItem('tasks',JSON.stringify(taskList));
+        }
+        
+        
+        //Adding Simple Validation
         if(task == '')
         {
             alert("Task field is required")
@@ -82,7 +168,7 @@ $(document).ready(function() {
             }
             
             //Create a task list
-            var taskList = JSON.parse(localStorage.getItem('tasks'));
+            //var taskList = JSON.parse(localStorage.getItem('tasks'));
             
             //New Task Object creation
             var newTask = {
@@ -103,3 +189,41 @@ $(document).ready(function() {
     }
     
 });
+
+//Function to get a single task on pageLoad for edit task page
+function getTask()
+{
+    var $_GET = getQueryParams(document.location.search);
+    id = $_GET['id'];
+    
+    var taskList = JSON.parse(localStorage.getItem('tasks'));
+    
+    for (var i=0;i < taskList.length; i++)
+    {
+        if(taskList[i].id == id)
+        {
+            $('#edit-task-form #task-id').val(taskList[i].id);
+            $('#edit-task-form #task').val(taskList[i].task);
+            $('#edit-task-form #priority').val(taskList[i].task_priority);
+            $('#edit-task-form #date').val(taskList[i].task_date);
+            $('#edit-task-form #time').val(taskList[i].task_time);
+        }
+    }
+}
+
+//Function to Get the HTTP GET Requests
+function getQueryParams(qs)
+{
+    qs = qs.split("+").join(" ");
+    
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+    
+    while(tokens = re.exec(qs))
+    {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]); 
+    }
+    
+    return params;
+}
